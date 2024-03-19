6e35e75edd3ed62fe90dfbb901a3bf056ebc78b3
Alexey Staburov <gilbertfrost@yandex.ru>
2024-03-20 01:54:24 +0300

    done tg push

---
+++ src/telegram/telegramSendMessage.js
@@ -0,0 +1,21 @@
+import { API, TELEGRAM_CHAT_ID} from "@/lib/telegramApi";
+
+export async function sendPushMessage(data) {
+  const text = `Новая бронь! Не проеби, там ${data.person} человек в ${data.time} и возможно на сегодня. но это не точно....`;
+  try {
+    const response = await fetch(API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chat_id: TELEGRAM_CHAT_ID,
+        text,
+      }),
+    });
+    console.log("получилось");
+  } catch (error) {
+    // throw error;
+    console.log("не получилось", error);
+  }
+}
