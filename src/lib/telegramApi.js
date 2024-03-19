4b6ed7663989c6e1762508ca4976505fc759b3ec
Staburov Alexey <121376881+StaburovAlexey@users.noreply.github.com>
2024-03-20 02:37:40 +0300

    Delete src/lib directory

--- src/lib/telegramApi.js
+++
@@ -1,4 +0,0 @@
-export const TELEGRAM_TOKEN = "6437804151:AAFXWRIJjJmx-IWvSWSDLPwACX1RHK4GPo4";
-export const TELEGRAM_CHAT_ID = "@IggyBuddy";
-export const API= `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`
-
