e8b65f293e363ed5362b352a89ca6119fb3d9c7c
Alexey Staburov <gilbertfrost@yandex.ru>
2025-11-24 14:02:04 +0300

    Refactor FooterApp.vue to script setup

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="page-footer indigo darken-3">
     <div class="container black-text row">
-      <p class="col s6 white-text">Выбери дату:</p>
+      <p class="col s6 white-text">Выберите дату:</p>
       <div class="input-field col s6">
         <i class="material-icons prefix white-text">event</i>
         <input
@@ -17,51 +17,45 @@
   </footer>
 </template>
 
-<script>
+<script setup>
+import { onMounted, ref } from "vue";
+import { useStore } from "vuex";
 import M from "materialize-css/dist/js/materialize.min";
-export default {
-  data() {
-    return {
-      date: "",
-    };
-  },
-  watch: {},
-  async mounted() {
-    const inst = await M.Datepicker.init(this.$refs.datepicker, {
-      format: "dd.mm.yy",
-      autoClose: true,
-      firstDay: 1,
-      // container:
-    });
-    inst.setDate(new Date());
-    this.date = this.formatDate(new Date());
-    this.$store.commit("setDate", this.date);
-    try {
-      await this.$store.dispatch("fetchInfo");
-    } catch (e) {
-      console.log(e);
-    }
-  },
-  methods: {
-    async updateDate() {
-      this.date = this.$refs.datepicker.value;
-      try {
-        this.$store.commit("setDate", this.date);
-        await this.$store.dispatch("fetchInfo");
-      } catch (e) {
-        console.log(e);
-      }
-    },
-    formatDate(date) {
-      const options = {
-        day: "2-digit",
-        month: "2-digit",
-        year: "2-digit",
-      };
-      return new Intl.DateTimeFormat("ru-RU", options).format(date);
-    },
-  },
+
+const store = useStore();
+const datepicker = ref(null);
+const date = ref("");
+let pickerInstance = null;
+
+const formatDate = (value) => {
+  const options = { day: "2-digit", month: "2-digit", year: "2-digit" };
+  return new Intl.DateTimeFormat("ru-RU", options).format(value);
 };
+
+const syncDate = async () => {
+  date.value = datepicker.value?.value || "";
+  if (!date.value) return;
+  try {
+    store.commit("setDate", date.value);
+    await store.dispatch("fetchInfo");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+onMounted(() => {
+  pickerInstance = M.Datepicker.init(datepicker.value, {
+    format: "dd.mm.yy",
+    autoClose: true,
+    firstDay: 1,
+  });
+  pickerInstance.setDate(new Date());
+  date.value = formatDate(new Date());
+  store.commit("setDate", date.value);
+  store.dispatch("fetchInfo").catch((error) => console.log(error));
+});
+
+const updateDate = () => syncDate();
 </script>
 
 <style lang="scss"></style>
