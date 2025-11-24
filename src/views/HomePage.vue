f03a442138088da7c90d5701f45763cf70e4d525
Alexey Staburov <gilbertfrost@yandex.ru>
2025-11-24 15:09:42 +0300

    Wire NavBar open-room to BarTheme

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,17 @@
 <template>
-  <NavBar />
-  <BarTheme />
+  <NavBar @open-room="openRoom" />
+  <BarTheme ref="bar" />
   <FooterApp />
 </template>
 
 <script setup>
+import { ref } from "vue";
 import BarTheme from "@/components/BarTheme.vue";
 import FooterApp from "@/components/FooterApp.vue";
 import NavBar from "@/components/NavBar.vue";
+
+const bar = ref(null);
+const openRoom = () => bar.value?.openRoom();
 </script>
 
 <style lang="scss" scoped></style>
